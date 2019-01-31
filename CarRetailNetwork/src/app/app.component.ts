1bbeda72bbcc4516858d50def5d14587f3cf6af3
cloned2k16 <paolo@yaoo.net>
2019-01-31 23:20:08 +0100

    lets shape it up ..

--- CarRetailNetwork/src/app/app.component.ts
+++ CarRetailNetwork/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector:       'app-root',
+  templateUrl:    './app.component.html',
+  styleUrls:      ['./app.component.less']
 })
+
 export class AppComponent {
   title = 'CarRetailNetwork';
+  now: number;
+
+  constructor() {
+    setInterval(() => { this.now = Date.now(); }, 500);
+  }
 }
--- CarRetailNetwork/src/app/app.module.ts
+++ CarRetailNetwork/src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }                 from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { MatToolbarModule }         from '@angular/material/toolbar';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import { AppComponent }             from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
