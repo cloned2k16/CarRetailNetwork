af7d7055a3da77b78547babd15eccbf28fc6d0c5
cloned2k16 <paolo@yaoo.net>
2019-02-01 11:05:39 +0100

    adding some shape ..

--- CarRetailNetwork/src/app/app.component.ts
+++ CarRetailNetwork/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit}                   from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+export interface Section {
+  name: string;
+  address: string;
+}
 
 @Component({
   selector:       'app-root',
@@ -6,11 +12,29 @@ import { Component } from '@angular/core';
   styleUrls:      ['./app.component.less']
 })
 
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'CarRetailNetwork';
   now: number;
 
-  constructor() {
+  dealers: Section[]  = [ { name: "Dream Cars",     address: "somewhere" }
+                        , { name: "Super Cars",     address: "Somewhere else" }
+                        , { name: "Bargain Cars",   address: " via Cavour" }];
+
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
     setInterval(() => { this.now = Date.now(); }, 500);
+  }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 }
--- CarRetailNetwork/src/app/app.module.ts
+++ CarRetailNetwork/src/app/app.module.ts
@@ -1,7 +1,58 @@
-import { NgModule }                 from '@angular/core';
+//import 'core-js/es6/reflect';
+//import 'core-js/es7/reflect';
+//import 'zone.js/dist/zone';
+//import 'hammerjs';
+//import 'web-animations-js';
+
+import { NgModule } from '@angular/core';
 import { BrowserModule }            from '@angular/platform-browser';
-import { MatToolbarModule }         from '@angular/material/toolbar';
 import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { A11yModule } from '@angular/cdk/a11y';
+
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+} from '@angular/material';
 
 import { AppComponent }             from './app.component';
 
@@ -12,9 +63,53 @@ import { AppComponent }             from './app.component';
   imports: [
     BrowserModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    A11yModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    ScrollingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
